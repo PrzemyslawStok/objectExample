149786f8fdc6fc4289bf05cea25a00afa7390045
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-04-05 18:45:05 +0200

    cd..

--- src/main/java/com/example/objectExample/SimpleController.java
+++ src/main/java/com/example/objectExample/SimpleController.java
@@ -1,21 +1,29 @@
 package com.example.objectExample;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SimpleController {
-    int[][] table = {{1,2,3,4},{1,2,3,4},{1,2,3,4},{1,2,3,4}};
+    int[][] table = {{1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}};
+
     @RequestMapping("/")
     @ResponseBody
-    String getInfo(){
+    String getInfo() {
         return "info...";
     }
 
     @RequestMapping("table")
     @ResponseBody
-    int [][] getTable(){
+    int[][] getTable() {
         return table;
+    }
+
+    @RequestMapping("tableElement/{i}/{j}")
+    @ResponseBody
+    String getTableElement(@PathVariable("i") Integer i, @PathVariable("j") Integer j) {
+        return "tablica[" + i + "," + j + "]=" + table[i][j];
     }
 }
