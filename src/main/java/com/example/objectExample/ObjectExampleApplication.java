f6d300da187b5a0f161c18dbbe4307d9a143b895
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-04-05 17:51:12 +0200

    init

---
+++ src/main/java/com/example/objectExample/ObjectExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.objectExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/objectExample/ObjectExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.objectExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
