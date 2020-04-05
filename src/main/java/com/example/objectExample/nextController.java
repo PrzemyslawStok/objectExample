26eb528b5a6057dc86f98e631fb7c3e0cac5620a
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-04-05 19:12:36 +0200

    cd..

--- src/main/java/com/example/objectExample/nextController.java
+++ src/main/java/com/example/objectExample/nextController.java
@@ -1,5 +1,6 @@
 package com.example.objectExample;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class nextController {
+    @Autowired StudentRepository repository;
+
     @RequestMapping("student/{id}")
     @ResponseBody
     Student getStudent(){
