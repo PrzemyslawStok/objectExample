3e205630563b1297e747f8cc61297a4294612c00
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-04-05 19:10:54 +0200

    cd..

--- src/main/java/com/example/objectExample/Student.java
+++ src/main/java/com/example/objectExample/Student.java
@@ -2,12 +2,22 @@ package com.example.objectExample;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @Data
+@Entity
 public class Student {
     String name;
     String surname;
 
-    Integer id;
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    public Student(){
+    }
 
     Student(String name, String surname){
         this.name = name;
---
+++ src/main/java/com/example/objectExample/StudentRepository.java
@@ -0,0 +1,6 @@
+package com.example.objectExample;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface StudentRepository extends CrudRepository<Student,Integer> {
+}
